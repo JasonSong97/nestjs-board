0daba75356f6a0c20c602d662d4952e2db8e706a
Jason_song97 <jaegeunsong97@gmail.com>
2023-10-13 17:54:29 +0900

    feat : 특정 게시물 상태 업데이트

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
@@ -14,9 +14,7 @@ export class BoardsController {
      }
      
      @Post()
-     createBoard(
-          @Body() createBoardDto: CreateBoardDto
-          ): Board  {
+     createBoard(@Body() createBoardDto: CreateBoardDto): Board  {
           return this.boardsService.createBoard(createBoardDto);
      }
 
@@ -28,5 +26,10 @@ export class BoardsController {
      @Delete('/:id')
      deleteBoard(@Param('id') id: string): void {
           this.boardsService.deleteBoard(id)
+     }
+
+     @Patch('/:id/status')
+     updateBoardStatus(@Param('id') id: string, @Body('status') status: BoardStatus) {
+          return this.boardsService.updateBoardStatus(id, status);
      }
 }
