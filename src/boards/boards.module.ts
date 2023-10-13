64bf9ed0040dc613e3f55cc91dbcc3debc763259
Jason_song97 <jaegeunsong97@gmail.com>
2023-10-13 20:25:10 +0900

    feat : Repository 생성하기

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
 
  // 등록
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([BoardRepository])
+  ],
   controllers: [BoardsController],
   providers: [BoardsService]
 })
